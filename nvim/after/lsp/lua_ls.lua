b2f1b92f2b3fb455e46279f1357189eac1f0ac15
yuji0276 <yujikami0110@gmail.com>
2026-09-02 23:27:26 +0900

    lspの導入

---
+++ nvim/after/lsp/lua_ls.lua
@@ -0,0 +1,12 @@
+---@type vim.lsp.Config
+return {
+  settings = {
+    Lua = {
+      runtime = { version = "LuaJIT" },
+      workspace = {
+        library = { vim.env.VIMRUNTIME },
+        checkThirdParty = false,
+      },
+    },
+  },
+}
