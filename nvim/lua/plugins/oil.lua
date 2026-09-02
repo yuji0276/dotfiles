14323de04c8af6e93f3975a0fb66affdf39bbabe
yuji0276 <yujikami0110@gmail.com>
2026-09-02 21:01:57 +0900

    ドットファイルを表示させる

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -2,7 +2,11 @@ return {
   'stevearc/oil.nvim',
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
+  opts = {
+    view_options = {
+      show_hidden = true,
+    },
+  },
   dependencies = { { "nvim-mini/mini.icons", opts = {} } },
   lazy = false,
   keys ={
