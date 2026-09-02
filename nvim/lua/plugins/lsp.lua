5c101f250440459dc320a0cd33efad35d9da2f0e
yuji0276 <yujikami0110@gmail.com>
2026-09-02 23:29:13 +0900

    lspの大元を追加

---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+    ensure_installed ={"lua_ls","gopls"},
+  },
+    dependencies = {
+      { "mason-org/mason.nvim", opts = {} },
+        "neovim/nvim-lspconfig",
+        "saghen/blink.cmp",
+      },
+  },
+}
