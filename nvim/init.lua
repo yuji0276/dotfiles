474695950c2be4d15954d8a979f08879e73ce034
yuji0276 <yujikami0110@gmail.com>
2026-09-02 23:27:50 +0900

    インデントなどの基本的な設定

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,13 @@
 require("config.lazy")
 
 vim.opt.number = true
+
+vim.opt.expandtab = true
+
+vim.opt.tabstop = 2
+
+vim.opt.shiftwidth = 2
+
+vim.opt.softtabstop = 2
+
+vim.opt.clipboard:append('unnamedplus')
